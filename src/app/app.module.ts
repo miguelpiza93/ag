6801bf4f2793b697074b2b9dc78fde81268dfb33
CN\pizami <miguel.piza@grupocnet.com>
2018-09-14 16:19:03 -0500

    Se refactoriza el index para separarlo por componentes angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeAboutComponent } from './home-about/home-about.component';
+import { BannerAreaComponent } from './banner-area/banner-area.component';
+import { HomeServicesComponent } from './home-services/home-services.component';
+import { HomeVideoComponent } from './home-video/home-video.component';
+import { HomeReviewComponent } from './home-review/home-review.component';
+import { HomeBlogComponent } from './home-blog/home-blog.component';
+import { HomeFooterComponent } from './home-footer/home-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeAboutComponent,
+    BannerAreaComponent,
+    HomeServicesComponent,
+    HomeVideoComponent,
+    HomeReviewComponent,
+    HomeBlogComponent,
+    HomeFooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/home-review/home-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-review',
+  templateUrl: './home-review.component.html',
+  styleUrls: ['./home-review.component.css']
+})
+export class HomeReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
